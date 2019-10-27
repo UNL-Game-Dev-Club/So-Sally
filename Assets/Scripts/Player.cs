c7a1e49a0277feaa1c1e2585116f5f21955e91b9
Robert Lafferty <rclafferty@huskers.unl.edu>
2019-10-26 21:23:34 -0500

    Player movement, Cube light turns on/off

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    Rigidbody2D thisRigidbody;
+    const float speed = 0.1f;
+
+    bool ringIsLit;
+
+    [SerializeField] GameObject sallyCube;
+    SpriteRenderer cubeRenderer;
+
+    [SerializeField] Sprite litRingInside;
+    [SerializeField] Sprite unlitRingInside;
+
+    [SerializeField] Sprite litRingOutside;
+    [SerializeField] Sprite unlitRingOutside;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        thisRigidbody = GetComponent<Rigidbody2D>();
+
+        if (thisRigidbody == null)
+        {
+            thisRigidbody = this.gameObject.AddComponent<Rigidbody2D>();
+        }
+
+        ringIsLit = false;
+        cubeRenderer = sallyCube.GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        if (Mathf.Abs(horizontal) > 0.5f)
+        {
+            thisRigidbody.transform.Translate(Vector2.right * horizontal * speed);
+        }
+
+        if (Input.GetButtonDown("Fire1"))
+        {
+            if (ringIsLit)
+            {
+                StopListening();
+            }
+            else
+            {
+                Listen();
+            }
+        }
+    }
+
+    void Listen()
+    {
+        ringIsLit = true;
+        cubeRenderer.sprite = litRingInside;
+    }
+
+    void StopListening()
+    {
+        ringIsLit = false;
+        cubeRenderer.sprite = unlitRingInside;
+    }
+}
---
+++ Assets/Scripts/SmartSally.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SmartSally : MonoBehaviour
+{
+    bool ringIsLit;
+
+    [SerializeField] Sprite litRingInside;
+    [SerializeField] Sprite unlitRingInside;
+
+    [SerializeField] Sprite litRingOutside;
+    [SerializeField] Sprite unlitRingOutside;
+    SpriteRenderer cubeRenderer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        ringIsLit = false;
+
+        cubeRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            if (ringIsLit)
+            {
+                StopListening();
+            }
+            else
+            {
+                Listen();
+            }
+        }
+    }
+
+    void Listen()
+    {
+        ringIsLit = true;
+        cubeRenderer.sprite = litRingInside;
+    }
+
+    void StopListening()
+    {
+        ringIsLit = false;
+        cubeRenderer.sprite = unlitRingInside;
+    }
+}
